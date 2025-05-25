975f239f84cd10c11420f9d98ce11ce2a0fccf90
Tienxinhtrai304 <vntien12345@gmail.com>
2025-05-25 16:20:22 +0700

    Update THANHTOAN.cpp

--- THANHTOAN.cpp
+++ THANHTOAN.cpp
@@ -1,41 +1,46 @@
-#include <iostream>
 #include "ThanhToan.h"
-using namespace std;
-ThanhToan::ThanhToan(string l, double p, string mt) : loai(l), phi(p), moTa(mt) {}
 
+ThanhToan::ThanhToan(string l, double p, string mt) : loai(l), phi(p), moTa(mt) {}
 ThanhToan::~ThanhToan() {}
 
-void ThanhToan::thanhToan(double tien) const {
-    cout << "Thanh toan bang " << loai << ", so tien: " <<  tien + phi << endl;
-}
-
 void ThanhToan::hienThiPhi() const {
-    cout << "Phi: " << phi << " - Mo ta: " << moTa << endl;
+    cout << "Loai: " << loai << ", Phi: " << phi << ", Mo ta: " << moTa << endl;
 }
 
 string ThanhToan::getLoai() const {
     return loai;
 }
+ThanhToan* ThanhToan::operator+(const ThanhToan& t) const {
 
-ThanhToan ThanhToan::operator+(const ThanhToan& t) const {
-    return ThanhToan(loai + " + " + t.loai, phi + t.phi, moTa + " & " + t.moTa);
+    ThanhToanTienMat* result = new ThanhToanTienMat();
+    result->loai = loai + "+" + t.loai;
+    result->phi = phi + t.phi;
+    result->moTa = moTa + "; " + t.moTa;
+    result->setSoTienMatKhachDua(0); 
+    return result;
 }
-int main() {
-  
-    ThanhToan dienThoai("Tien mat", 0, "Thanh toan khi mua dien thoai");
 
-    ThanhToan laptop("The tin dung", 15000, "Thanh toan khi mua laptop");
+ThanhToanTienMat::ThanhToanTienMat(double phi, double tienKhachDua)
+    : ThanhToan("Tien Mat", phi, "Thanh toan truc tiep bang tien mat"),
+      soTienMatKhachDua(tienKhachDua) {}
 
-    dienThoai.thanhToan(300000); 
-    laptop.thanhToan(400000);   
-
-    laptop.hienThiPhi();
-
-    ThanhToan tong = dienThoai + laptop;
-
-    tong.hienThiPhi();
-    tong.thanhToan(700000); 
-    return 0;
+void ThanhToanTienMat::thanhToan(double tien) const {
+    cout << "Thanh toan tien mat: Tong tien = " << tien + phi << " VND" << endl;
+    cout << "So tien khach dua: " << soTienMatKhachDua << " VND" << endl;
+    cout << "Tien thoi lai: " << (soTienMatKhachDua - (tien + phi)) << " VND" << endl;
 }
+
+void ThanhToanTienMat::hienThiPhi() const {
+    ThanhToan::hienThiPhi();
+}
+
+void ThanhToanTienMat::setSoTienMatKhachDua(double tien) {
+    soTienMatKhachDua = tien;
+}
+
+double ThanhToanTienMat::getSoTienMatKhachDua() const {
+    return soTienMatKhachDua;
+}
+
 
 
